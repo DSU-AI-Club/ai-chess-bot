4a407110ef45d8d7c4ba343518b5b75a92c094a0
Burke Johnson <185158560+synth-mania@users.noreply.github.com>
2025-10-22 18:11:07 -0500

    update disclaimer

--- demo-bot/train.py
+++ demo-bot/train.py
@@ -4,3 +4,9 @@ def train():
 # This is just a placeholder
 # Your implementation of the chess bot
 # may or may not require a training script
+# in any case, train() should be implemented if you
+# are using the __main__.py which was provided,
+# or your bot must otherwise gracefully handle the "train"
+# command line option to avoid running into errors as we
+# systematically run each bot with the "train" option
+# prior to the tourney
